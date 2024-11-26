9fbd9ce9df9b0cca6d577581683566af6471f97c
Ana Carolina <73427901+AnaFigueredo@users.noreply.github.com>
2024-11-26 20:19:54 -0300

    Update CorsConfiguration.java
    Remoção de comentários

--- FATEC2024_CRUD/Funcionario-Crud/src/main/java/fatec/poo/Funcionario_Crud/configuration/CorsConfiguration.java
+++ FATEC2024_CRUD/Funcionario-Crud/src/main/java/fatec/poo/Funcionario_Crud/configuration/CorsConfiguration.java
@@ -10,10 +10,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**") // Permite requisições em todos os endpoints do backend
-                .allowedOrigins("http://localhost:63342") // URL do frontend executado pelo IntelliJ
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD") // Métodos HTTP permitidos
-                .allowedHeaders("*") // Permite todos os cabeçalhos
-                .allowCredentials(true); // Permite envio de cookies (se necessário)
+        registry.addMapping("/**") 
+                .allowedOrigins("http://localhost:63342") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD") 
+                .allowedHeaders("*") 
+                .allowCredentials(true); 
     }
 }
