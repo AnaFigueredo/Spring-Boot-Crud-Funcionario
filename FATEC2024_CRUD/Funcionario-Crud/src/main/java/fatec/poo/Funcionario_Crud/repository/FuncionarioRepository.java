eda569a10bd4b4549f380b1d3fbe0888a4451e9f
Ana Carolina <73427901+AnaFigueredo@users.noreply.github.com>
2024-11-26 20:21:06 -0300

    Update FuncionarioRepository.java
    Remoção de comentários

--- FATEC2024_CRUD/Funcionario-Crud/src/main/java/fatec/poo/Funcionario_Crud/repository/FuncionarioRepository.java
+++ FATEC2024_CRUD/Funcionario-Crud/src/main/java/fatec/poo/Funcionario_Crud/repository/FuncionarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // A interface FuncionarioRepository estende JpaRepository, que já fornece os métodos de CRUD
 public interface FuncionarioRepository extends JpaRepository<Funcionario,Integer> {
-    // Aqui podemos adicionar métodos personalizados, caso necessário
+    
 }
